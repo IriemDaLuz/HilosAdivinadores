07ec6317c83e699640e0c6b6d6495a04e6425a1b
IriemDlzGlnd <yenduuca2005@gmail.com>
2025-02-18 08:05:48 +0000

    Update HiloAdivinador.java

--- HilosAdivinadores/src/hilosadivinadores/HiloAdivinador.java
+++ HilosAdivinadores/src/hilosadivinadores/HiloAdivinador.java
@@ -1,13 +1,42 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package hilosadivinadores;
 
-/**
- *
- * @author Iriem ST
- */
-public class HiloAdivinador {
-    
+import java.util.Random;
+
+
+public class HiloAdivinador implements Runnable {
+
+    private final String nombreHilo;
+    private final NumeroOculto numeroOculto;
+
+
+    public HiloAdivinador(String nombreHilo, NumeroOculto numeroOculto) {
+        this.nombreHilo = nombreHilo;
+        this.numeroOculto = numeroOculto;
+    }
+
+    @Override
+    public void run() {
+        Random r = new Random();
+        boolean acertado = false;
+
+        while (!acertado) {
+            int numPropuesto = r.nextInt(101); 
+            int resultado = numeroOculto.propuestaNumero(numPropuesto);
+
+            if (resultado == -1) {
+                System.out.println(nombreHilo + " detenido. El número ya fue adivinado.");
+                return;
+            } else if (resultado == 1) {
+                System.out.println(nombreHilo + " adivinó el número oculto. (" + numPropuesto + ")");
+                acertado = true;
+            } else {
+                System.out.println(nombreHilo + " dice que es el " + numPropuesto + ". Incorrecto.");
+            }
+
+            try {
+                Thread.sleep(50);
+            } catch (InterruptedException e) {
+            }
+        }
+    }
 }
